bdbe846d3961bdc41842a856eafb94a4e546367d
Ronnel <ron@24digital.com.au>
2016-07-14 23:05:07 +0800

    sched

--- app/Http/Controllers/CourtController.php
+++ app/Http/Controllers/CourtController.php
@@ -32,8 +32,8 @@ class CourtController extends Controller
                             ->leftJoin('court_details','court_details.id','=','schedules.court_detail_id')
                             ->leftJoin('courts','courts.id','=','court_details.court_id')
                             ->leftJoin('associates','associates.id','=','schedules.associate_id')
-                            ->where('service_requests.insert_time','LIKE', '%'.$input['date'].'%')
-                            ->where('courts.id',)
+                            ->where('service_requests.update_time','LIKE', '%'.$input['date'].'%')
+                            ->where('courts.id',$input['court_id'])
                             ->select('associates.*')
                             ->get();
 
