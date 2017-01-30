989a17054053b6ddb20fae54c701de1bdd1d3eb4
Ronnel <ron@24digital.com.au>
2017-01-30 22:04:13 +0800

    service

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -18,18 +18,30 @@ class ScheduleController extends Controller
         try {
             $input = \Request::only('associate_id', 'court_detail_id', 'from_date', 'to_date');
             if ($input['court_detail_id'] == 0) {
-                $result = Schedule::where('service_request_assignees.associate_id', $input['associate_id'])
-                    ->leftJoin('court_details', 'schedules.court_detail_id', '=', 'court_details.id')
-                    ->leftJoin('courts', 'court_details.court_id', '=', 'courts.id')
+                // $result = Schedule::where('service_request_assignees.associate_id', $input['associate_id'])
+                //     ->leftJoin('court_details', 'schedules.court_detail_id', '=', 'court_details.id')
+                //     ->leftJoin('courts', 'court_details.court_id', '=', 'courts.id')
+                //
+                //     ->leftJoin('service_requests','schedules.id', '=', 'service_requests.schedule_id')
+                //     ->leftJoin('service_request_assignees','service_requests.id', '=', 'service_request_assignees.service_request_id')
+                //
+                //     ->leftJoin('request_types', 'schedules.request_type_id', '=', 'request_types.id')
+                //     ->leftJoin('associates', 'service_request_assignees.associate_id', '=','associates.id')
+                //     ->whereBetween('schedules.date_time', array($input['from_date'], $input['to_date']))
+                //     ->select('schedules.*', 'courts.name AS court_name', 'request_types.name AS request_type','associates.fullname')
+                //     ->get();
+                 $sched = Schedule::where('service_request_assignees.associate_id', $input['associate_id'])
+                         ->whereBetween('schedules.date_time', array($input['from_date'], $input['to_date']))
+                         ->select('schedules.*')->get();
 
-                    ->leftJoin('service_requests','schedules.id', '=', 'service_requests.schedule_id')
-                    ->leftJoin('service_request_assignees','service_requests.id', '=', 'service_request_assignees.service_request_id')
+                 $service =  Service::where('assigned_associate_id',$input['associate_id'])
+                             ->whereBetween('schedules.date_time', array($input['from_date'], $input['to_date']))
+                             ->select('service_requests.*')
+                     ->get();
 
-                    ->leftJoin('request_types', 'schedules.request_type_id', '=', 'request_types.id')
-                    ->leftJoin('associates', 'service_request_assignees.associate_id', '=','associates.id')
-                    ->whereBetween('schedules.date_time', array($input['from_date'], $input['to_date']))
-                    ->select('schedules.*', 'courts.name AS court_name', 'request_types.name AS request_type','associates.fullname')
-                    ->get();
+                $result = ['schedules' => $sched, 'service_requests' => $sched];
+
+
             } elseif ($input['associate_id'] == 0) {
                 $result = Schedule::where('court_detail_id', $input['court_detail_id'])
                     ->leftJoin('court_details', 'schedules.court_detail_id', '=', 'court_details.id')
